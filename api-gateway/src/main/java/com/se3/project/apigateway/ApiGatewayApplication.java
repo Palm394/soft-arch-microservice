7b6a7a0dfbd91567aac09bc016a017e8ff3f184b
Palm394 <palm.nichapat@gmail.com>
2023-11-10 21:29:37 +0700

    fix multiple allow headers cors

--- api-gateway/src/main/java/com/se3/project/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/se3/project/apigateway/ApiGatewayApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,22 +20,26 @@ public class ApiGatewayApplication {
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(r -> r.path("/user-service/**")
-						.filters(f -> f.rewritePath("^/user-service", ""))
+						.filters(f -> f
+								.rewritePath("^/user-service", "")
+								.dedupeResponseHeader("Access-Control-Allow-Origin","RETAIN_UNIQUE"))
 						.uri("lb://USER-SERVICE"))
 				.route(r -> r.path("/reservation-service/**")
-						.filters(f -> f.rewritePath("^/reservation-service", ""))
+						.filters(f -> f
+								.rewritePath("^/reservation-service", "")
+								.dedupeResponseHeader("Access-Control-Allow-Origin","RETAIN_UNIQUE"))
 						.uri("lb://RESERVATION-SERVICE"))
 				.route(r -> r.path("/parking-lots-service/**")
-						.filters(f -> f.rewritePath("^/parking-lots-service", ""))
+						.filters(f -> f
+								.rewritePath("^/parking-lots-service", "")
+								.dedupeResponseHeader("Access-Control-Allow-Origin","RETAIN_UNIQUE"))
 						.uri("lb://PARKING-LOTS-SERVICE"))
 				.route(r -> r.path("/review-service/**")
-						.filters(f -> f.rewritePath("^/review-service", ""))
+						.filters(f -> f
+								.rewritePath("^/review-service", "")
+								.dedupeResponseHeader("Access-Control-Allow-Origin","RETAIN_UNIQUE"))
 						.uri("lb://REVIEW-SERVICE"))
 				.build();
-	}
-	@GetMapping("/service")
-	public String dump() throws Exception {
-		return "dumP";
 	}
 
 	public static void main(String[] args) {
---
+++ api-gateway/src/main/java/com/se3/project/apigateway/PreFlightCorsConfiguration.java
@@ -0,0 +1,29 @@
+package com.se3.project.apigateway;
+
+import jakarta.ws.rs.HttpMethod;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class PreFlightCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        return new CorsWebFilter(corsConfigurationSource());
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        config.addAllowedMethod(HttpMethod.PUT);
+        config.addAllowedMethod(HttpMethod.POST);
+        config.addAllowedMethod(HttpMethod.DELETE);
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
